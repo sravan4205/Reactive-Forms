08643ea3d7c6248adeaaf8beafff9d2d7aceab34
sravan4205 <sravan.amishetti@gmail.com>
2020-01-09 16:24:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from './helpers/must-match.validator';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'project-y';
+@Component({ selector: 'app-root', templateUrl: 'app.component.html'
+ })
+export class AppComponent implements OnInit {
+    registerForm: FormGroup;
+    submitted = false;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            title: ['', Validators.required],
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required],
+            acceptTerms: [false, Validators.requiredTrue]
+        }, {
+            validator: MustMatch('password', 'confirmPassword')
+        });
+    }
+
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+    }
+
+    onReset() {
+        this.submitted = false;
+        this.registerForm.reset();
+    }
 }
